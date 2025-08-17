f2ad26ba5723283979639b4dbec1d78b7a49d0b6
KimMyungHoon0933 <aronzny@gmail.com>
2025-08-17 23:13:16 +0900

    메인 html상에 내용을 next.js상에 구현했습니다.

---
+++ next.js_mainlayout/src/app/page.js
@@ -0,0 +1,10 @@
+
+export default async function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      hello next.js
+       return <h1 className={children}>About</h1>;
+       
+    </div>
+  );
+}
