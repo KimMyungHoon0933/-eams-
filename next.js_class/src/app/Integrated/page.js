dc032f0822774bfdea5d071e9eb225391fbee9d1
KimMyungHoon0933 <aronzny@gmail.com>
2025-09-15 12:53:39 +0900

    Add files via upload

---
+++ next.js_class/src/app/Integrated/page.js
@@ -0,0 +1,15 @@
+// 그대로 유지
+import AccordionSidebar from "../components/AccordionSidebar";
+import "./UnvierFuction_menu.css";
+
+export default function Page() {
+  return (
+    <div className="route-integrated">
+      <div className="integrated-container">
+        <div className="integrated-body">
+          <AccordionSidebar />
+        </div>
+      </div>
+    </div>
+  );
+}
