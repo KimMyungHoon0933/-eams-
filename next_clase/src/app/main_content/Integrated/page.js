ee8776ab357c4a84e1c2329c08f950f0b9ab8f1e
KimMyungHoon0933 <aronzny@gmail.com>
2025-10-27 18:32:29 +0900

    Delete next_clase/src/app directory

--- next_clase/src/app/main_content/Integrated/page.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import IntegratedMenu from "../components/IntegratedMenu";
-
-export default function Page() {
-  // 예: 특정 메뉴를 기본으로 열고 싶으면 initialKey 전달
-  // const initial = "휴학 > 복학 신청";
-  return <IntegratedMenu /* initialKey={initial} */ />;
-}
