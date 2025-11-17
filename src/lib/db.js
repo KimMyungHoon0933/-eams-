8af59957ed25a04f4068a405b5fbd40df2cda10d
ty0209 <ttsunn12@naver.com>
2025-11-17 18:46:31 +0900

    Delete src directory

--- src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = mysql.createPool({
-  host: "localhost",
-  user: "testuser",
-  password: "1234",
-  database: 'univer',
-  charset: 'utf8mb4',
-});
